883dff12a4b23231ec2cc2846335bb79a0ab3005
Jakub <49901045+gorsett@users.noreply.github.com>
2020-09-28 19:38:49 +0200

    Paths
    - first path
    - removing duplicated projectiles

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+
+    //conf params
+    [SerializeField] List<Transform> waypoints;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

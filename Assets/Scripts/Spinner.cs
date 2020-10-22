66ad173b57abc15c202d151dde01486fbdbda7d5
Jakub <49901045+gorsett@users.noreply.github.com>
2020-10-22 21:47:18 +0200

    Final changes
    - paths redone
    - waves redone
    Game finalized

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float speedofSpin = 2.0f;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, speedofSpin * Time.deltaTime, Space.World);
+    }
+}
